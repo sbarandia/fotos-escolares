9080495bfe4b479e4b9a81b739bdff28fe5df13d
Santiago Barandiaran <sbarandia@gmail.com>
2026-07-12 20:32:33 -0300

    Primera versión del sitio

---
+++ docs/js/app.js
@@ -0,0 +1,9 @@
+console.log("Proyecto iniciado");
+
+document
+    .getElementById("btnPrueba")
+    .addEventListener("click", function () {
+
+        alert("JavaScript funcionando correctamente");
+
+    });
